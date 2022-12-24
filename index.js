85fa1c79e8d5c8bb56e062bd4ac71377ed171d24
jervincarlos <jervin@sliqpay.com>
2022-12-24 14:20:04 +0800

    package

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 // Middleware
 app.use(express.json());
